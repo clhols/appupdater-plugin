baa5c59c9acdca24225ee05ba49db9c41efe48c8
Claus Holst <clhols@gmail.com>
2019-11-15 19:37:47 +0100

    Update for GitHubPackages

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,8 +16,18 @@ repositories {
 }
 
 publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/clhols/appupdater-plugin")
+            credentials {
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
+                password = project.findProperty("gpr.key") as String? ?: System.getenv("PASSWORD")
+            }
+        }
+    }
     publications {
-        create("appupdaterPlugin", MavenPublication::class.java) {
+        register("appupdaterPlugin", MavenPublication::class.java) {
             groupId = "dk.youtec"
             artifactId = "appupdater"
             version = "1.0.1"
