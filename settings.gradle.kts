49d4b081faed23c3ad40ad32105e0087e649f348
Claus Holst <clhols@gmail.com>
2019-11-20 21:43:08 +0100

    Added remote build cache

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,12 @@
 include(":")
+
+buildCache {
+    remote(HttpBuildCache::class.java) {
+        url = uri("http://192.168.87.91:5071/cache/")
+        isPush = System.getenv("CI") == "true"
+        credentials {
+            username = "youtec"
+            password = "QapcErxW2pSa3pR"
+        }
+    }
+}
