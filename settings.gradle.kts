c88a42e97cf1fb410a4f97a48ef5029beeb54e75
Claus Holst <clhols@gmail.com>
2021-07-10 21:14:36 +0200

    Gradle 7

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,6 +4,7 @@ buildCache {
     remote(HttpBuildCache::class.java) {
         url = uri("http://svn.youtec.dk:5071/cache/")
         isPush = System.getenv("CI") == "true"
+        isAllowInsecureProtocol = true
         credentials {
             username = "youtec"
             password = "QapcErxW2pSa3pR"
